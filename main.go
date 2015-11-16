0de0161a057c20d1b9b5e27633cbbc58aa2de822
kusabashira <kusabashira227@gmail.com>
2015-11-16 23:43:23 +0900

    Implement command as rough

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/yuya-takeyama/argf"
+)
+
+func printErr(err error) {
+	fmt.Fprintln(os.Stderr, "rl:", err)
+}
+
+func reverse(s string) string {
+	a := []rune(s)
+	b := make([]rune, len(a))
+	for i := 0; i < len(a); i++ {
+		b[len(a)-i-1] = a[i]
+	}
+	return string(b)
+}
+
+func do(r io.Reader) error {
+	b := bufio.NewScanner(r)
+	for b.Scan() {
+		fmt.Println(reverse(b.Text()))
+	}
+	return b.Err()
+}
+
+func _main() int {
+	r, err := argf.From(os.Args[1:])
+	if err != nil {
+		printErr(err)
+		return 2
+	}
+
+	if err = do(r); err != nil {
+		printErr(err)
+		return 1
+	}
+	return 0
+}
+
+func main() {
+	e := _main()
+	os.Exit(e)
+}
