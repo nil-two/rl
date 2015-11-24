6ef02b72fe1ba7713fd26d7be87a9199bce9f4e8
kusabashira <kusabashira227@gmail.com>
2015-11-24 14:16:23 +0900

    Implement Reverser.Reverse

--- reverse.go
+++ reverse.go
@@ -6,3 +6,12 @@ type Reverser struct {
 func NewReverser() *Reverser {
 	return &Reverser{}
 }
+
+func (r *Reverser) Reverse(s string) string {
+	a := []rune(s)
+	b := make([]rune, len(a))
+	for i := 0; i < len(a); i++ {
+		b[len(a)-i-1] = a[i]
+	}
+	return string(b)
+}
