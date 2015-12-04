f4460e826bc5439cf0ec38874f468752fb0d53cd
kusabashira <kusabashira227@gmail.com>
2015-12-04 15:21:08 +0900

    Add .travis.yml

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ Usage: rl [OPTION]... [FILE]...
 Reverse lines of FILE(s), or standard input.
 
 Options:
-  -d, --delimiter=DELIM    delimit line by DELIM
+  -d, --delimiter=DELIM    delimit lines by DELIM
       --help               display this help text and exit
       --version            output version information and exit
 `[1:])
